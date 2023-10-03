052ff3a658bd18048312137b97cbf94ccd5a4678
Bruno <yatosad@gmail.com>
2023-10-03 20:29:34 -0300

    Ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -25,5 +25,5 @@ function compilaSass() {
 exports.default = function() {
     gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass))
     gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJS))
-    gulp.watch('./source/images/*.', { ignoreInitial: false }, gulp.series(comprimeImg))
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImg))
 }
